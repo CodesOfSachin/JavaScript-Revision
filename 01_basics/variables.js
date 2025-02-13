9b4e7945787853ef018df43e1abfb249c1aa7b7c
CodesOfSachin <codes.of.sachin@gmail.com>
2025-02-13 11:09:54 +0000

    Basic data types and console method to print values

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const userId = 3242
+let userEmail = "sachin@gmail.com"
+var userPassword = "1235"
+userCity = "London"
+let userState; //it will print undefined
+
+/*
+    userId = 5342 // Not allowed due to const 
+    Prefer not to use var 
+    because of issue in block scope and functional scope
+*/
+
+console.log(userId, userEmail); // print userId and userEmail in single line
+console.table([userId, userEmail, userPassword, userCity, userState]) // prints every mentioned variable in a table form
+
