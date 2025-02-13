8b23eda8741f1f2cdb507ccbda8dfc819860d3a2
CodesOfSachin <codes.of.sachin@gmail.com>
2025-02-13 12:24:56 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) // works only in browser not in nodejs enviroment
+
+console.log(3 + 3)
+console.log("Sachin") // code readability should be high
+
+let name = "Sachin"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// Number => maximum size of a number 2 to Power 53
+// bigint => for larger number 
+// string => for text and number
+// null => standlone value and object
+// undefined => variable which is not assigned with value for example state
+// symbol => unique
+
+// object
+
+console.log(typeof "sachin")
+console.log(typeof age)
