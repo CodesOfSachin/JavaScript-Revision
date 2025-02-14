b1a3f1e113920d55090036c8c81e527d650d31e6
CodesOfSachin <codes.of.sachin@gmail.com>
2025-02-14 08:07:32 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+let name = "sachin"
+
+console.log(typeof score);
+console.log(typeof name);
+
+let valueInNumber_01 = Number(score)
+let valueInNumber = Number(name)
+console.log(typeof valueInNumber_01);
+console.log(valueInNumber_01);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "sachin" => NaN
+// true => 1; false => 0
+
+let isLoggenIn = "sachin"
+
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sachin" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
