2c0a4f7e138fbd59196cabd9d717d21efcc165d6
CodesOfSachin <codes.of.sachin@gmail.com>
2025-02-18 10:26:17 +0000

    while and do while loops

---
+++ 05_Iterations/02.js
@@ -0,0 +1,22 @@
+let index = 0
+while (index <= 10) {
+    //console.log(index)
+    index += 2
+}
+
+let myArray = ["Brago", "Bari", "Sachin"]
+
+let arr = 0
+while(arr < myArray.length) {
+    //console.log(`Name is ${myArray[arr]}`);
+    arr++
+}
+
+
+// in do while loop loop executes atleast one time then it checks condition
+let score = 0
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while(score <= 10)
